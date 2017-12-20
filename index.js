608249877871aa951ed052be45a6062fe36134c5
Dorian Taing <33088182+Doriantaing@users.noreply.github.com>
2017-12-20 14:27:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,90 +1,44 @@
+var srcMovie = data.films[0].img;
 
-<html>
+var div = document.createElement('div');
+div.setAttribute('class', 'content');
+document.body.appendChild(div);
 
-<head>
-  <meta charset="utf-8">
-  <title></title>
-  <style media="screen">
-    .modal {
-      display: none;
-      position: fixed;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-    }
+for (var i = 0; i < srcMovie.length; i++) {
+  var img = document.createElement('img');
+  img.setAttribute('class', 'affiche');
+  div.appendChild(img);
+  var hal = data.films[i].img;
+  img.src = hal;
+}
 
-    .modal_overlay {
-      position: fixed;
-      z-index: -1;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background: rgba(0, 0, 0, 0.4);
-    }
-
-    .modal_content {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      width: 70%;
-      height: 75vh;
-      margin: 20px auto;
-      background: #fff;
-    }
-  </style>
-</head>
-
-<body>
-    
-    
-  <!-- MODAL -->
-
-  <div class="modal hidden">
-    <div class="modal_overlay"></div>
-    <div class="modal_content">
-      <h4>TITRE FILM</h4>
-      <p class="modal_player">PLAYER</p>
-      <p>FICHE DETAILS FILM</p>
-    </div>
-  </div>
+let modolo = document.querySelectorAll('.affiche');
+var overlay = document.querySelector('.modal_overlay');
+var modal = document.querySelector('.modal');
 
 
+for (var i = 0; i < modolo.length; i++) {
+  modolo[i].addEventListener('click', function() {
+    var title = document.querySelector('.modal h4');
+    var player = document.querySelector('.modal .modal_player')
+    modal.style.display = 'block';
+  })
+}
 
-  <script type="text/javascript" src="js/data.js"></script>
-  <script type="text/javascript" src="js/main.js"></script>
-  <script type="text/javascript">
-    
-    
-    /** MODAL : LECTEUR + FICHE FILM **/
+document.querySelector('.modal_overlay').addEventListener('click', function() {
+  modal.style.display = '';
+})
 
-    let btn = document.querySelectorAll('.cat-Tous');
-    let overlay = document.querySelector('.modal_overlay');
-    let modal = document.querySelector('.modal');
-    let posModal = 0;
+window.addEventListener('keyup', function(event) {
+  if (event.which === 27) {
+    modal.style.display = '';
+  }
+})
 
 
-    for (let i = 0; i < btn.length; i++) {
-      btn[i].addEventListener('click', function() {
-        let title = document.querySelector('.modal h4');
-        let player = document.querySelector('.modal .modal_player');
-        modal.style.display = 'block';
-      })
-    }
+window.addEventListener('scroll', function() {
+  if (posModal) {
 
-    document.querySelector('.modal_overlay').addEventListener('click', function() {
-      modal.style.display = '';
-    })
-
-    window.addEventListener('scroll', function() {
-      if (posModal) {
-
-      }
-      modal.style.display = '';
-    })
-  </script>
-</body>
-
-</html>
+  }
+  modal.style.display = '';
+})
